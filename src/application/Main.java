0898e70e78368e003c88c495f2c5af12c79d6aff
hemingyu <hemingyu@localhost>
2022-07-19 18:19:30 -0600

    Another change.

--- src/application/Main.java
+++ src/application/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 		try {
 			VBox root = new VBox();
 			root.getChildren().add(new Label("Mohammad's label"));
-			root.getChildren().add(new Label("Mingyu's Label"));
+			root.getChildren().add(new Label("Mingyu's Label2"));
 			Scene scene = new Scene(root,400,400);
 			
 			primaryStage.setScene(scene);
